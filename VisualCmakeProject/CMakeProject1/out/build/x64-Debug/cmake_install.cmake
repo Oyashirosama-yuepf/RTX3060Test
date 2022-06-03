a90504365d308e4e285931cd2917a5010c70d580
yuepengfei <yuepengfei@holomatic.com>
2022-06-03 14:55:10 +0800

    ADD: cmakelist， find_package和add_directories对比

--- VisualCmakeProject/CMakeProject1/out/build/x64-Debug/cmake_install.cmake
+++ VisualCmakeProject/CMakeProject1/out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/GitProject/VisualCmake/CMakeProject1
+# Install script for directory: D:/开源库/Documents/VisualCmakeProject/CMakeProject1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "D:/GitProject/VisualCmake/CMakeProject1/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "D:/开源库/Documents/VisualCmakeProject/CMakeProject1/out/install/x64-debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,12 +34,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/GitProject/VisualCmake/CMakeProject1/out/build/x64-Debug/AStarLib/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("D:/GitProject/VisualCmake/CMakeProject1/out/build/x64-Debug/Hanoi/cmake_install.cmake")
+  include("D:/开源库/Documents/VisualCmakeProject/CMakeProject1/out/build/x64-Debug/AStarLib/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/GitProject/VisualCmake/CMakeProject1/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/开源库/Documents/VisualCmakeProject/CMakeProject1/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
